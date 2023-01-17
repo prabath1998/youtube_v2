e185c331dbfc523f4584c1676d28559fadc09f54
prabath1998 <prabhathudayanga1998@gmail.com>
2023-01-18 00:27:48 +0530

    bug fix on search

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import {useState,useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import {Videos,ChannelCard} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const {id} = useParams();
+
+  console.log(channelDetail,videos);
+
+  useEffect(() => {
+   fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]));
+
+   fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items));
+  }, [id]);
+
+  
+  
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          height:'300px',
+          background: 'linear-gradient(90deg, rgba(204,30,219,1) 0%, rgba(231,222,29,1) 55%, rgba(255,128,0,1) 100%)',
+          zIndex: 10,
+        }} />
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+      </Box>
+      <Box p={2} display="flex">
+      <Box sx={{ mr: { sm: '100px' } }}/>
+        <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -18,7 +18,7 @@ const Feed = () => {
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
-          Copyright © 2023 Prabath Udayanga
+          Copyright © 2023 Prabath 
         </Typography>
       </Box>
       <Box p={2} sx={{ overflow: 'auto', height: '90vh', flex: 2 }}>
