72ec2fc0c926aa00d789b9126e7b367aaf1a0920
prabath1998 <prabhathudayanga1998@gmail.com>
2023-01-17 14:30:09 +0530

    custom components added

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Paper, IconButton } from '@mui/material';
+import { SearchIcon } from '@mui/icons-material';
+import { borderRadius } from '@mui/system';
+
+const SearchBar = () => {
+  return (
+    <Paper component="form" onSubmit={() =>{}} sx={{borderRadius:20}}>Test</Paper>
+  )
+}
+
+export default SearchBar
